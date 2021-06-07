195f1a34caf30e48fd88c260d50e319c98511fe3
Caia Chuang <caiachuang@gmail.com>
2021-06-07 03:36:04 -0700

    Add react router.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, useHistory } from "react-router-dom";
+
 
 const App = () => {
-  return (
-    <div className="app">
-
-    </div>
-  );
+    return (
+        <Router>
+            <div className="app">
+                <Switch>
+                    <Route></Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
