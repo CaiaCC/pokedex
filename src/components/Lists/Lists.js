007838c209932c70afe33824d264d72f6aa7f1e6
Caia Chuang <caiachuang@gmail.com>
2021-06-07 06:38:39 -0700

    Create List component.

---
+++ src/components/Lists/Lists.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import './Lists.css';
+
+const Lists = () => {
+    const [pokemonLists, setPokemonLists] = useState({});
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Lists;
